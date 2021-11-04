f0a3d45ce9e80244744a1f24fdafd8daef6f930a
elrick97 <ricardo.rmz.97@hotmail.com>
2021-11-04 01:18:56 -0600

    global variables generan cuadruplos

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -52,8 +52,17 @@ class EzParser extends EmbeddedActionsParser {
 
         // Optional Initialization
         this.OPTION2(() => {
-          this.CONSUME(Lexer.Equals)
-          this.OR([{ ALT: () => this.SUBRULE(this.literal) }, { ALT: () => this.SUBRULE(this.constantArray) }])
+          this.ACTION(() => this.symbolTable.pushOperand(varName))
+          const operator = this.CONSUME(Lexer.Equals).image as '='
+          this.ACTION(() => this.symbolTable.pushOperator(operator))
+          this.OR([{
+            ALT: () => {
+
+              const { value, type } = this.SUBRULE(this.literal)
+              this.ACTION(() => this.symbolTable.pushLiteral(value, type))
+            }
+          }, { ALT: () => this.SUBRULE(this.constantArray) }])
+          this.ACTION(() => this.symbolTable.doAssignmentOperation())
         })
       },
       SEP: Lexer.Comma,
